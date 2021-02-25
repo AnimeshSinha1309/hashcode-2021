12188a34bfbf3423e6b1c36d7cfd4b6f4604a62e
yoogottamk <yoogottamk@outlook.com>
2021-02-26 01:25:01 +0530

    [weighted perfect run] +solutions

---
+++ hashcode/yoogottamk.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+import numpy as np
+
+from hashcode.utils import load, save
+
+
+def solve(pid):
+    gs = load(pid)
+
+    intersection_times = defaultdict(lambda: defaultdict(float))
+    intersection_counts = defaultdict(int)
+
+    for car_path in gs.car_paths:
+        t = 0
+        for c in car_path:
+            street = gs.streets[c]
+            intersection_times[street.end][c] += 1 - (t / gs.D)
+            intersection_counts[street.end] += 1
+            t += street.l
+
+    for k, v in intersection_times.items():
+        norm = np.array(list(v.values())).sum()
+        for _v in v.keys():
+            v[_v] /= norm
+
+    output = {}
+
+    for k in intersection_counts.keys():
+        v = intersection_times[k]
+        for _v in v.keys():
+            v[_v] *= intersection_counts[k]
+            v[_v] = np.ceil(v[_v]).astype("int")
+
+        output[k] = list(v.items())
+
+    return output, gs.street_names
+
+
+for pid in range(6):
+    save(pid, *solve(pid))
