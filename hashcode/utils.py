1c58a9c31dcc18af6fcb341960046b1080ae9fb6
Gaurang Tandon <gaurang.tandon@students.iiit.ac.in>
2021-02-25 23:43:46 +0530

    Added reader and loader

--- hashcode/utils.py
+++ hashcode/utils.py
@@ -1,8 +1,34 @@
-problem_names = ['A', 'B', 'C', 'D', 'E']
+problem_names = ['a', 'b', 'c', 'd', 'e', 'f']
 
 
 def load(pid):
-    pass
+    file_path = f"problem/{problem_names[pid]}"
+
+    with open(file_path) as f:
+        content = f.read().split("\n")
+        d, i, s, c, f = list(map(int, content[0].split(" ")))
+
+        streets = []
+        street_names = []
+        street_name_to_idx = {}
+        car_paths = []
+
+        street_content = content[1:s + 1]
+        car_content = content[s + 1:s + c + 1]
+
+        for idx, line in enumerate(street_content):
+            vals = line.split(" ")
+            start, end, name, l = int(vals[0]), int(vals[1]), vals[2], int(vals[3])
+            street_names.append(name)
+            streets.append((start, end, l))
+            street_name_to_idx[name] = idx
+
+        for line in car_content:
+            streets = line.split(" ")[1:]
+            streets = [street_name_to_idx[x] for x in streets]
+            car_paths.append(streets)
+
+    return GameState(streets, street_names, street_name_to_idx, car_paths, i, f, d)
 
 
 def save(pid, solution):
